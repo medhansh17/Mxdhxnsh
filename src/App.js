9f670590e570afeba22609a4f5f9708bd8a699ed
Medhansh Jain <medhanshjain2010@gmail.com>
2024-02-28 11:08:24 +0530

    Feat : added homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { Cursor } from "react-creative-cursor";
+import Navbar from "./Components/navbar/navbar.component";
+import "react-creative-cursor/dist/styles.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Hero">
+      <Cursor isGelly={true} cursorBackgrounColor="purple" />
+      <Navbar />
+      <div className="heroTitle">
+        <div className="primaryTitle six-caps-regular">
+          <span className="box">M</span>
+          <span>E</span>
+          <span>D</span>
+          <span>H</span>
+          <span>A</span>
+          <span>N</span>
+          <span>S</span>
+          <span>H</span>
+        </div>
+        <div className="titleDesc">
+          <p>
+            HEY, I AM A FRONT-END WEB DEVELOPER SKILLED AT CRAFTING DIGITAL
+            EXPERIENCES THAT ARE AESTHETIC, EFFECTIVE, AND PURELY ENGAGING FOR
+            USERS.
+          </p>
+        </div>
+      </div>
+      <div className="heroFooter">
+        <span>Scroll to Explore</span>
+      </div>
     </div>
   );
 }
